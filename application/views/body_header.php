5b09b9d0edf9a3dee319ef7a2d4ef16cdd1e1c61
Kevin <digitalkdogg@gmail.com>
2017-06-23 19:59:31 -0500

    fixed table and mobile bg bug

--- application/views/body_header.php
+++ application/views/body_header.php
@@ -1,12 +1,12 @@
 	<div class = "row" id = "header-container">
-			<div class = "col-sm-12 col-lg-1" id = "social-container">
+			<div class = "col-xs-12 col-sm-1 col-md-1 col-lg-1" id = "social-container">
 				<a class = "fa fa-envelope sociallink" href = "#"></a>
 				<a class = "fa fa-facebook sociallink" href = "#"></a>
 				<a class = "fa fa-github-alt sociallink" href = "#"></a>
 			</div>
-			<div class = "col-sm-12 col-lg-9" id = "contact-container">
-			<?php 
-				foreach ($query as $row) 
+			<div class = "col-xs-12 col-sm-9 col-md-9 col-lg-9" id = "contact-container">
+			<?php
+				foreach ($query as $row)
 				{ ?> <h2><?php echo $row->Firstname . ' ' . $row->Lastname;	?> </h2>
         		    <li><b>Web Designer / Web Progammer</b></li>
         		    <li><?php echo $row->Email; ?> </li>
@@ -15,7 +15,7 @@
         		}
 				?>
 			</div>
-	
-			<div class = "col-sm-12 col-lg-2" id = "photo-container">
+
+			<div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2" id = "photo-container">
 				<img src = "http://lorempixel.com/400/200/" />
 			</div>
--- application/views/nav_menu.php
+++ application/views/nav_menu.php
@@ -1,11 +1,11 @@
-		<div class = "col-lg-12" id= "menu-container">
+		<div class = "col-sm-12 col-md-12 col-lg-12 col-xl-12" id= "menu-container">
 			<nav class="navbar navbar-inverse">
 			  <div class="container-fluid">
 			    <div class="navbar-header">
 			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span> 
+			        <span class="icon-bar"></span>
 			      </button>
 			      <a class="navbar-brand mobile-hidden" href="#">Menu</a>
 			    </div>
@@ -13,8 +13,8 @@
 			      <ul class="nav navbar-nav">
 			        <li id = "about" class="active"><a href="#">About</a></li>
 			        <li id = "education"><a href="#">Education</a></li>
-			        <li id = "experience"><a href="#">Experience</a></li> 
-			        <li id = "skills"><a href="#">Skills</a></li> 
+			        <li id = "experience"><a href="#">Experience</a></li>
+			        <li id = "skills"><a href="#">Skills</a></li>
 			      </ul>
 			      <ul class="nav navbar-nav navbar-right">
 			        <li><a href="#" id = "contact" data-toggle="modal" data-target="#myModal">
