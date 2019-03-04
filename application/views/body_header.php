3dcc684a3d294d4811b3fd66fa1abaceb6b2c741
Kevin Bollman <kevinbollman@gmail.com>
2019-03-03 18:17:34 -0600

    Change clip to reperesent the code clip shape

--- application/views/body_header.php
+++ application/views/body_header.php
@@ -1,15 +1,23 @@
 	<div class = "row" id = "header-container">
 			<div class = "col-xs-12 col-sm-1 col-md-1 col-lg-1" id = "social-container">
-				<a class = "fa fa-envelope sociallink" href = "#"></a>
-				<a class = "fa fa-facebook sociallink" href = "#"></a>
-				<a class = "fa fa-github-alt sociallink" href = "#"></a>
+				<div class = "social-wrap">
+				<!--	<a class = "fa fa-envelope sociallink" href = "#">E</a> -->
+				<a class = "sociallink" href = "#"><span class = "fa fa-envelope"></span></a>
+				</div>
+				<div class = "social-wrap">
+					<a class = "sociallink" href = "#"><span class = "fa fa-facebook"></span></a>
+				</div>
+				<div class = "social-wrap">
+					<a class = "sociallink" href = "#"><span class = "fa fa-github-alt"></span></a>
+				</div>
+
 			</div>
 			<div class = "col-xs-12 col-sm-9 col-md-9 col-lg-9" id = "contact-container">
 				<div class = "col-xs-8">
 			<?php
 				foreach ($query as $row)
 				{ ?> <h2><?php echo $row->Firstname . ' ' . $row->Lastname;	?> </h2>
-        		    <li><b>Web Designer / Web Progammer</b></li>
+        		    <li><b>Full Stack Web Developer</b></li>
         		    <li><?php echo $row->Email; ?> </li>
         		    <li><?php echo $row->Phone; ?> </li>
         		<?php
