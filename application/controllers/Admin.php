7f3fc8c7d21b853aa850849990a9e677a961722d
Kevin Bollman <kevinbollman@gmail.com>
2019-03-15 16:53:27 -0500

    added check to make sure there is data before displaying the guts page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -109,8 +109,9 @@ class Admin extends CI_Controller {
         if ($resumeid != null ) {  
   	     	$this->load->model('Section_Details');
   	     	$meta = array("data" => $this->Section_Details->get_by_user($resumeid, $type));
-          $this->load->view('admin/body_details', array("meta" => $meta));
-
+          if (sizeof($meta['data']) != 0) {
+            $this->load->view('admin/body_details', array("meta" => $meta));
+          }
   	     	$this->load->view('admin/close_body');
         }
 	    } else {
